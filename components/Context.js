ab4ab1a50903bd0bc681b5a581a5b27025b930f5
LeThanh <kzxlmdtn888@gmail.com>
2022-08-12 13:36:08 +0700

    nearLy finish home

--- components/Context.js
+++ components/Context.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 export const AuthorSong = React.createContext();
 export const AudioSong = React.createContext();
+export const Location = React.createContext();
 
 
 
--- components/authorPlayList/index.js
+++ components/authorPlayList/index.js
@@ -5,13 +5,17 @@ import {useContext} from "react";
 import {AuthorSong} from "../Context";
 
 
+
 function AuthorPlayList() {
     const {authorSong} = useContext(AuthorSong);
 
     return (
             <div>
                 <DashboardContent>
-                    <h1 style={{color: "White"}}>Artist: {authorSong[0].author}</h1>
+                    <div style={{display: 'flex'}}>
+                        <img src={authorSong[0].links.images[0].url} alt="11" width="40%" style={{height: "300px"}}/>
+                        <h1 style={{color: "White", display: "flex", justifyContent: "center", alignItems: "end"}}>Artist: {authorSong[0].author}</h1>
+                    </div>
                     <PlayListAuthor/>
                 </DashboardContent>
             </div>
