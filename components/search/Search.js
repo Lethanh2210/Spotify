2c861eee06d128c05b67c7c57423405f9abfb793
LeThanh <kzxlmdtn888@gmail.com>
2022-08-15 17:27:46 +0700

    finish

--- components/search/Search.js
+++ components/search/Search.js
@@ -3,18 +3,21 @@ import axios from "axios";
 import {CCard, CCardBody, CCardImage, CCardText} from "@coreui/react";
 import {Grid} from "@mui/material";
 import Link from "next/link";
+import {useContext} from "react";
+import {Location} from "../Context"
 
 export default function Search() {
     const [search, setSearch] = useState('');
-    const [token, setToken] = useState('');
+
     const [artists, setArtists] = useState([]);
     const [categories, setCategories] = useState([]);
-    const [isLoading, setIsLoading] = useState(false)
+    const [isLoading, setIsLoading] = useState(false);
+    const [token, setToken] = useContext(Location);
 
-    useEffect(() => {
-        let token = window.localStorage.getItem("token");
-        setToken(token)
-    }, [token])
+    // useEffect(() => {
+    //     let token = window.localStorage.getItem("token");
+    //     setToken(token)
+    // }, [token])
     useEffect(async () => {
         setIsLoading(true)
         const {data} = await axios.get("https://api.spotify.com/v1/browse/categories?limit=48&offset=0", {
@@ -40,8 +43,6 @@ export default function Search() {
                 type: "track,artist",
             }
         })
-
-
         if(data){
             setArtists(data.artists.items)
             setIsLoading(false)
